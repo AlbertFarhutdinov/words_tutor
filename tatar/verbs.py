a47d5dc71c288d232c4f52a6bdbae99f111336c0
AlbertFarkhutdinov <farhutdinovalbert@yandex.ru>
2023-11-21 21:59:20 +0400

    added tatar lessons

---
+++ tatar/verbs.py
@@ -0,0 +1,13 @@
+class Verb:
+    
+    def __init__(
+        self,
+        imperative: str,
+        tense: str = 'present',
+        is_negative: bool = False,
+        is_question: bool = False,
+    ):
+        self.imperative = imperative
+        self.tense = tense
+        self.is_negative = is_negative
+        self.is_question = is_question
